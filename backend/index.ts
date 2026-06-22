9bfa6833b0b825d33a9afe7515cdb34d33f88685
abhas-paul <apodul456@gmail.com>
2026-06-22 22:04:15 +0530

    Refactor database connection and update dependencies in package.json

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,5 @@
 import "dotenv/config";
 import app from "./src/app";
-import { connectDB } from "./src/config/db";
-
-await connectDB();
 
 export default {
   port: Number(process.env.PORT) || 3000,
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,15 @@
 import { Hono } from "hono";
+import { connectDB } from "../src/config/db.ts";
 
 const app = new Hono();
+
+const response_db = await connectDB();
 
 app.get("/", (c) => {
   return c.json({
     success: true,
     message: "API Running",
+    response_db
   });
 });
 
